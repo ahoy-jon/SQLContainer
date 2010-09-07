b2cc3083b45d841f0e0e57a6fef7dc0285ce301d
teppo.kurki@itmill.com <teppo.kurki@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2010-09-07 13:05:18 +0000

    Added the new tests to the set of all tests.

--- tests/src/com/vaadin/addon/sqlcontainer/AllTests.java
+++ tests/src/com/vaadin/addon/sqlcontainer/AllTests.java
@@ -6,10 +6,13 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.vaadin.addon.sqlcontainer.connection.SimpleJDBCConnectionPoolTest;
 import com.vaadin.addon.sqlcontainer.query.FreeformQueryTest;
+import com.vaadin.addon.sqlcontainer.query.TableQueryTest;
+import com.vaadin.addon.sqlcontainer.query.generator.SQLGeneratorsTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SimpleJDBCConnectionPoolTest.class, FreeformQueryTest.class,
-        RowIdTest.class, SQLContainerTest.class, ColumnPropertyTest.class })
+@SuiteClasses( { SimpleJDBCConnectionPoolTest.class, FreeformQueryTest.class,
+        RowIdTest.class, SQLContainerTest.class, ColumnPropertyTest.class,
+        TableQueryTest.class, SQLGeneratorsTest.class })
 public class AllTests {
 
 }
