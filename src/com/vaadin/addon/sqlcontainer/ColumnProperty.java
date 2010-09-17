9315f54eee673bd1361e60d23eb88466e8c7a035
teppo.kurki@itmill.com <teppo.kurki@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2010-09-17 07:38:07 +0000

    - Added conversion from java.util.Date to java.sql.Time/java.sql.TimeStamp/java.sql.Date as required by the type of the property. - Added NotNullableException that is thrown if one tries to set null value to a non-nullable column.

--- src/com/vaadin/addon/sqlcontainer/ColumnProperty.java
+++ src/com/vaadin/addon/sqlcontainer/ColumnProperty.java
@@ -1,6 +1,9 @@
 package com.vaadin.addon.sqlcontainer;
 
 import java.lang.reflect.Constructor;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 import com.vaadin.data.Property;
 
@@ -66,29 +69,68 @@ final public class ColumnProperty implements Property {
     public void setValue(Object newValue) throws ReadOnlyException,
             ConversionException {
         if (newValue == null && !nullable) {
-            throw new ConversionException(
-                    "Null values not allowed for this property.");
+            throw new NotNullableException(
+                    "Null values are not allowed for this property.");
         }
         if (readOnly) {
             throw new ReadOnlyException(
                     "Cannot set value for read-only property.");
         }
-        if (newValue != null
-                && !getType().isAssignableFrom(newValue.getClass())) {
-            try {
-                final Constructor<?> constr = getType().getConstructor(
-                        new Class[] { String.class });
-                newValue = constr.newInstance(new Object[] { newValue
-                        .toString() });
-            } catch (Exception e) {
-                throw new ConversionException(e);
+
+        /* Check if this property is a date property. */
+        boolean isDateProperty = Time.class.equals(getType())
+                || Date.class.equals(getType())
+                || Timestamp.class.equals(getType());
+
+        if (newValue != null) {
+            /* Handle SQL dates, times and Timestamps given as java.util.Date */
+            if (isDateProperty) {
+                /*
+                 * Try to get the millisecond value from the new value of this
+                 * property. Possible type to convert from is java.util.Date.
+                 */
+                long millis = 0;
+                if (newValue instanceof java.util.Date) {
+                    millis = ((java.util.Date) newValue).getTime();
+                    /*
+                     * Create the new object based on the millisecond value,
+                     * according to the type of this property.
+                     */
+                    if (Time.class.equals(getType())) {
+                        newValue = new Time(millis);
+                    } else if (Date.class.equals(getType())) {
+                        newValue = new Date(millis);
+                    } else if (Timestamp.class.equals(getType())) {
+                        newValue = new Timestamp(millis);
+                    }
+                }
+            }
+
+            /*
+             * If the type is not correct, try to generate it through a possibly
+             * existing String constructor.
+             */
+            if (!getType().isAssignableFrom(newValue.getClass())) {
+                try {
+                    final Constructor<?> constr = getType().getConstructor(
+                            new Class[] { String.class });
+                    newValue = constr.newInstance(new Object[] { newValue
+                            .toString() });
+                } catch (Exception e) {
+                    throw new ConversionException(e);
+                }
+            }
+
+            /*
+             * If the value to be set is the same that has already been set, do
+             * not set it again.
+             */
+            if (newValue.equals(value)) {
+                return;
             }
         }
 
-        if (newValue != null && newValue.equals(value)) {
-            return;
-        }
-
+        /* Set the new value and notify container of the change. */
         changedValue = newValue;
         owner.getContainer().itemChangeNotification(owner);
         modified = true;
@@ -150,5 +192,43 @@ final public class ColumnProperty implements Property {
 
     public boolean isNullable() {
         return nullable;
+    }
+
+    /**
+     * An exception that signals that a <code>null</code> value was passed to
+     * the <code>setValue</code> method, but the value of this property can not
+     * be set to <code>null</code>.
+     */
+    @SuppressWarnings("serial")
+    public class NotNullableException extends RuntimeException {
+
+        /**
+         * Constructs a new <code>NotNullableException</code> without a detail
+         * message.
+         */
+        public NotNullableException() {
+        }
+
+        /**
+         * Constructs a new <code>NotNullableException</code> with the specified
+         * detail message.
+         * 
+         * @param msg
+         *            the detail message
+         */
+        public NotNullableException(String msg) {
+            super(msg);
+        }
+
+        /**
+         * Constructs a new <code>NotNullableException</code> from another
+         * exception.
+         * 
+         * @param cause
+         *            The cause of the failure
+         */
+        public NotNullableException(Throwable cause) {
+            super(cause);
+        }
     }
 }
